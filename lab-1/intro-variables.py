da3bc84f602b148c7426aa7b729ceb09f86693ce
Ahmed Mahmoud <qassas.ahmed@std.mans.edu.eg>
2024-09-17 04:27:43 +0300

    lab-1 is added

---
+++ lab-1/intro-variables.py
@@ -0,0 +1,20 @@
+# this is a comment
+
+age = 28
+fname = "John"
+lname = "Doe"
+
+print(fname + ' ' + lname + 'is ' + str(age) + ' years old')
+
+num1 = 3
+num2 = 2
+sum = num1 + num2 # 3 + 2 = 5
+sub = num1 - num2 # 3 - 2 = 1
+mul = num1 * num2 # 3 * 2 = 6
+div = num1 / num2 # 3 / 2 = 1.5
+exp = num1 ** num2 # 3 ^ 2 = 9
+print(f"num 1 = {num1} num 2 = {num2}")
+print(f"sum = {sum}\nsub = {sub}\nmul = {mul}\ndiv = {div}\n3^2 = {exp}")
+
+
+
